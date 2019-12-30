b2959aa141b2fff7c71c74fd0a537f91759fcb61
hhiroki <gyouzakuuka@gmail.com>
2019-12-30 11:54:54 +0900

    change MyApp to a StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
