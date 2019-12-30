e55a9fa7b6d914a8f5a91252c128167612f75777
hhiroki <gyouzakuuka@gmail.com>
2019-12-30 12:44:44 +0900

    add note key

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_midi/flutter_midi.dart';
+
+const BorderRadiusGeometry borderRadius = BorderRadius.only(
+    bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0)
+);
 
 void main() => runApp(MyApp());
 
@@ -19,22 +24,93 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  double get keyWidth => 80 + (80 * _widthRatio);
+  double _widthRatio = 0.0;
+  bool _showLabels = true;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Piano'),),
+        appBar: AppBar(
+          title: Text('Flutter Piano'),
+        ),
         drawer: Drawer(child: SafeArea(child: ListView(children: <Widget>[]))),
         body: ListView.builder(
           itemCount: 7,
           scrollDirection: Axis.horizontal,
           itemBuilder: (BuildContext context, int index) {
-            return Container();
+            final int i = index * 12;
+            return SafeArea(
+              child: Stack(
+                children: <Widget>[
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      _buildKey(24 + i, false),
+                      _buildKey(26 + i, false),
+                      _buildKey(28 + i, false),
+                      _buildKey(29 + i, false),
+                      _buildKey(31 + i, false),
+                      _buildKey(33 + i, false),
+                      _buildKey(35 + i, false),
+                    ],
+                  ),
+                  Positioned(
+                    left: 0.0,
+                    right: 0.0,
+                    bottom: 100.0,
+                    top: 0.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Container(
+                          width: keyWidth * .5,
+                        ),
+                        _buildKey(25 + i, true),
+                        _buildKey(27 + i, true),
+                        Container(
+                          width: keyWidth * .5,
+                        ),
+                        _buildKey(30 + i, true),
+                        _buildKey(32 + i, true),
+                        _buildKey(34 + i, true),
+                        Container(
+                          width: keyWidth * .5,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            );
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildKey(int midi, bool accidental) {
+    if (accidental) {
+      return Container(
+        width: keyWidth,
+        color: Colors.black,
+        margin: EdgeInsets.symmetric(horizontal: 2.0),
+        padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
+        child: Material(
+          elevation: 6.0,
+          borderRadius: borderRadius,
+          shadowColor: Color(0x802196F3),
+        ),
+      );
+    }
+    return Container(
+        width: keyWidth,
+        color: Colors.white,
+        margin: EdgeInsets.symmetric(horizontal: 2.0)
     );
   }
 }
